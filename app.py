1d6ca5396019acc3e560dcffb74a674091c4a40d
Gautham Krishnan <gauthamkrishnanpriya@gmail.com>
2023-06-29 16:12:06 +0530

    add: gradio interface

---
+++ app.py
@@ -0,0 +1,13 @@
+import gradio as gr
+import os
+from functions import *
+
+examples_dir = 'examples'
+title = "Birds Classification - ResNet34 PyTorch"
+examples = [os.path.join(examples_dir, i) for i in os.listdir('examples')]
+
+interface = gr.Interface(fn=predict, inputs=gr.Image(type= 'numpy', shape=(64, 64)).style(height= 256),
+            outputs= gr.Label(num_top_classes= 5), cache_examples= False,
+            examples= examples, title= title)
+
+interface.launch()
